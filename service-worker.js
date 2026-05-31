c918916c0545597846e450f48075fd46a0657d1e
investigator27 <mitchell.slaney@intrepid.ca>
2026-05-30 20:50:53 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* sw-revision: 27 — bump this comment when testing updates locally */
 const BASE = new URL('./', self.location).pathname;
-let activeCacheName = 'surveillance-travel-pwa-v128';
+let activeCacheName = 'surveillance-travel-pwa-v130';
 
 const CORE_ASSETS = [
   BASE + 'index.html',
